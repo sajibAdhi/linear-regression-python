0f387451fbd0479cd00b0f68f4dce9ab8c2ebf28
Sajib Adhikary <tosajibadhi@gmail.com>
2024-04-07 21:09:47 +0600

    :sparkles: independent-variables-and-visualizing-data
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 if __name__ == '__main__':
     df = pd.read_csv("salaries.csv")
-    print(df.head())
+
+    x = df.iloc[:, :-1].values  # get all rows with all columns except the last one #
+    y = df.iloc[:, -1].values  # get all rows with only the last column #
+
+    plt.scatter(x, y)
+    plt.show()
