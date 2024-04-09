2b01d6090fb2f6451c3df719fd182c8984854315
Sajib Adhikary <tosajibadhi@gmail.com>
2024-04-09 15:15:53 +0600

    :sparkles: split-into-training-and-testing-data
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
 
 if __name__ == '__main__':
     df = pd.read_csv("salaries.csv")
@@ -8,5 +10,10 @@ if __name__ == '__main__':
     x = df.iloc[:, :-1].values  # get all rows with all columns except the last one #
     y = df.iloc[:, -1].values  # get all rows with only the last column #
 
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
     plt.scatter(x, y)
     plt.show()
+
+    print(x_train.shape)
+    print(x_test.shape)
