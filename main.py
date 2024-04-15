4f3f2e1460982c3d46560195a81befe937a8f692
Sajib Adhikary <tosajibadhi@gmail.com>
2024-04-15 12:56:40 +0600

    :sparkles: model-training-prediction-evaluation
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
-
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
 if __name__ == '__main__':
     df = pd.read_csv("salaries.csv")
@@ -12,8 +13,15 @@ if __name__ == '__main__':
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-    plt.scatter(x, y)
-    plt.show()
+    model = LinearRegression()
+    model.fit(x_train, y_train)
 
-    print(x_train.shape)
-    print(x_test.shape)
+    y_pred = model.predict(x_test)
+
+    r2 = r2_score(y_test, y_pred)
+    print(f"R2 Score: {r2} ({r2:.3%})")
+
+    plt.scatter(x_test, y_test)
+    plt.plot(x_test, y_pred, color="yellow")
+
+    plt.show()
