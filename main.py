cc7c9f6dee0fe9f6a8cdbf43d61f3d9964fa40bc
Sajib Adhikary <tosajibadhi@gmail.com>
2024-04-25 10:34:36 +0600

    :sparkles: multiple-polynomial-regression
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
 if __name__ == '__main__':
-    df = pd.read_csv("salaries.csv")
+    df = pd.read_csv("salaries-cities.csv")
 
-    x = df.iloc[:, :-1].values  # get all rows with all columns except the last one #
-    y = df.iloc[:, -1].values  # get all rows with only the last column #
+    x = df.iloc[:, :-1].values  # Taking all columns except the last one, i.e. TWO columns
+    y = df.iloc[:, -1].values
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
     model = LinearRegression()
+
     model.fit(x_train, y_train)
 
     y_pred = model.predict(x_test)
-
     r2 = r2_score(y_test, y_pred)
-    print(f"R2 Score: {r2} ({r2:.3%})")
 
-    plt.scatter(x_test, y_test)
-    plt.plot(x_test, y_pred, color="yellow")
-
-    plt.show()
+    salaries = model.predict([[11, 1], [11, 2], [12, 1], [12, 2]])
+    print(salaries)
